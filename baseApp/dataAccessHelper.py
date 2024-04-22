81f3052e9cdb6deeaa09579cee54c584dcd51a8f
Yuuya Shimada <blk1012y2@gmail.com>
2024-04-23 01:52:41 +0900

    Added UserApp

--- baseApp/dataAccessHelper.py
+++ baseApp/dataAccessHelper.py
@@ -7,13 +7,20 @@ class dataAccessHelper():
     #名称検索データ
     get_posts_name = []
     #検索オブジェクト無しエラー
-    err_msg_sortnull = 'テストが見つかりませんでした'
+    err_msg_sortnull = '募集中のテストが見つかりませんでした'
 
     #テストタスク全件取得（条件：募集フラグ==True, 削除フラグ==Falese）
     #新規クエリ
     def getAllTask(cls):
         cls.get_posts = TestPost.objects.filter(TestPost.RecrutingPeriodFlg==True, TestPost.DelFlg==False).order_by(id)
-        return cls.get_posts
+
+        #取得データ存在有無
+        if cls.get_posts:
+            #取得データ返却
+            return cls.get_posts
+        #取得データ無し：エラーメッセージ返却
+        else:
+            return cls.err_msg_sortnull
     
     #--------------以降検索用--------------
     #名称検索（部分一致可:大小区別なし）
@@ -31,7 +38,7 @@ class dataAccessHelper():
             #取得データソート
             cls.get_posts.sort(key=lambda post:abs(now - TestPost.RecrutingPeriodSt))
         else:
-            #エラー時：エラーメッセージ返却
+            #エラー時：エラーメッセージ返却（取得データなし）
             return cls.err_msg_sortnull
         
     #募集期日検索（期日が遠い）
@@ -55,7 +62,7 @@ class dataAccessHelper():
             now = datetime.datetime.now()
             cls.get_posts.sort(key=lambda post:abs(now + TestPost.TestStart))
         else:
-            #エラー時：エラーメッセージ返却
+            #エラー時：エラーメッセージ返却（取得データなし）
             return cls.err_msg_sortnull
             
     #テスト期日検索（期日が遠い）
@@ -65,7 +72,7 @@ class dataAccessHelper():
             now = datetime.datetime.now()
             cls.get_posts.sort(key=lambda post:abs(now + TestPost.TestStart))
         else:
-            #エラー時：エラーメッセージ返却
+            #エラー時：エラーメッセージ返却（取得データなし）
             return cls.err_msg_sortnull
     
     #テスト種類検索
--- users/models.py
+++ users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
+from baseApp.models import TestPost
 
-# Create your models here.
+from datetime import date
+
+class Gender(models.TextChoices):
+    MAN     = 'Man'
+    WOMEN   = 'Women'
+    OTHER   = 'Other'
+
+class CustomUser (AbstractUser, PermissionsMixin):
+    """カスタムユーザークラス
+
+    Note:
+        ユーザー情報のクラス
+
+    Attributes:
+        AccountName(str):アカウント名(IDとは異なりユーザーが設定できる)
+        UserBirth(dt):ユーザー誕生日
+        UserGender(str):ユーザー性別
+        RunningTest(mtm):実行中テストタスク(テストの外部キーを使用)
+        DoneTest(mtm):完了テスト(テストの外部キーを使用)
+    """
+    #AccountName(str):アカウント名(IDとは異なりユーザーが設定できる)
+    AccountName = models.CharField(max_length=100)
+
+    #UserBirth(dt):ユーザー誕生日
+    UserBirth = models.DateField(blank=True, null=False)
+
+    #UserGender(str):ユーザー性別
+    GENDER_CHOICES = [(g.value, g.name) for g in Gender]
+    UserGender = models.CharField(max_length=50, choices=GENDER_CHOICES, default=Gender.OTHER)
+
+    #RunningTest(mtm):実行中テストタスク(テストの外部キーを使用)
+    RunningTest = models.ManyToManyField(TestPost, related_name='running_tests')
+
+    #DoneTest(mtm):完了テスト(テストの外部キーを使用)
+    DoneTest = models.ManyToManyField(TestPost, related_name='done_tests')
+
+    #年齢計算
+    def ageMath(self):
+        today = date.today()
+        #現在月日が記入月日より過ぎていれば0、以前であれば-1
+        age = today.year - self.UserBirth.year - ((today.month, today.day) < (self.UserBirth.month, self.UserBirth.day))
+        return age
+
+    def __str__(self):
+        return self.AccountName
+
