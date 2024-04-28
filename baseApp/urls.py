ab8d805b78da51546c1a22290bd60485e8000d0b
Yuuya Shimada <blk1012y2@gmail.com>
2024-04-28 18:11:15 +0900

    キャッシュクリア、再コミット&プッシュ

--- baseApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views.defalt import index
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('index/', index.as_view(), name='index')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- baseApp/views/auth/header.py
+++
@@ -1,3 +0,0 @@
-
-def header_img(request):
-    pass
