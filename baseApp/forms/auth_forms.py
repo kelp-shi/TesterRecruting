f8fc5a411bcec4e411a517c48f860f933474e866
Yuuya Shimada <blk1012y2@gmail.com>
2024-05-27 22:50:27 +0900

    authforms.py edit

--- baseApp/forms/auth_forms.py
+++ baseApp/forms/auth_forms.py
@@ -5,8 +5,6 @@ from ..models import CustomUser
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].label = "Password"
