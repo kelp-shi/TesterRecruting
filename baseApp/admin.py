57f450e5b9ec8df0f98cbdd1e6eb497bea74297f
Yuuya Shimada <blk1012y2@gmail.com>
2024-06-23 10:23:28 +0900

    index class変更、banner imageモデル追加

--- baseApp/admin.py
+++ baseApp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from baseApp.db.application.app_models import *
 from baseApp.models import CustomUser
 from baseApp.db.application.dm_models import *
+from baseApp.db.application.utillity_models import *
 
 
 admin.site.register(TestPost)
@@ -9,3 +10,8 @@ admin.site.register(JoinRequest)
 admin.site.register(CustomUser)
 admin.site.register(DmRoom)
 admin.site.register(Massage)
+
+class BannerImages(admin.ModelAdmin):
+    list_display = ('bannerTitle', 'bannerTitle', 'activeFlg')
+
+admin.site.register(BannerImg)
--- baseApp/views/utillity.py
+++ baseApp/views/utillity.py
@@ -1,3 +1,7 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from baseApp.db.application.app_models import TestPost
+from baseApp.models import CustomUser
 import random, string
 
 def randomString():
@@ -13,3 +17,22 @@ def randomNumver(length):
     """
     digits = ''.join(random.choices(string.digits, k=length))
     return digits
+
+class index(LoginRequiredMixin, TemplateView):
+    """
+    indexビュー
+    """
+
+    template_name = 'app/index.html'
+    
+
+    def get_queryset(self):
+        queryset = TestPost.objects.filter(RecrutingPeriodFlg=False).order_by('RecrutingPeriodSt')[:3]
+        return queryset
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user.is_authenticated
+
+        return context
