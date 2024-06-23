dba52828c20f9648acb455b1cd785186227de2e7
Yuuya Shimada <blk1012y2@gmail.com>
2024-06-23 10:55:53 +0900

    indexのバナー画像モデルを実装 & 保存処理実装

---
+++ baseApp/db/application/utillity_models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class BannerImg(models.Model):
+    """
+    index.htmlのBanner
+    """
+    bannerTitle = models.CharField(max_length=255)
+    bannerImg = models.ImageField(upload_to='baseApp/images/org/banner', null=False)
+    activeFlg = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.activeFlg:
+            BannerImg.objects.filter(activeFlg=True).update(activeFlg=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.bannerTitle
