13401ed931d934766bc551a1f552ca11fec64100
Yuuya Shimada <blk1012y2@gmail.com>
2024-06-29 12:26:05 +0900

    index.html処理修正

--- baseApp/templatetags/custom_filters.py
+++ baseApp/templatetags/custom_filters.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def dict_key(value, key):
-    return value.get(key)
+def get_item(dictionary, key):
+    return dictionary.get(key)
--- baseApp/views/utillity.py
+++ baseApp/views/utillity.py
@@ -1,9 +1,12 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from baseApp.db.application.app_models import TestPost
-from baseApp.db.application.dm_models import DmRoom
+from baseApp.db.application.dm_models import DmRoom, Massage
 from baseApp.models import CustomUser
 import random, string
+import logging
+
+logger = logging.getLogger(__name__)
 
 def randomString():
     """
@@ -27,20 +30,31 @@ class index(LoginRequiredMixin, TemplateView):
     template_name = 'app/index.html'
 
     def get_context_data(self, **kwargs):
+        # 自身の情報
+        current_user = self.request.user
         context = super().get_context_data(**kwargs)
         # ログインユーザ情報
-        context['user'] = self.request.user
-        # 新規ポスト(３件)
+        context['user'] = current_user
+        # 新規ポスト(3件)
         context['newposts'] = TestPost.objects.filter(RecrutingPeriodFlg=True).order_by('RecrutingPeriodSt')[:3]
         # おすすめポスト(10件)
         context['recomendpost'] = TestPost.objects.filter(RecrutingPeriodFlg=True).order_by('?')[:10]
         # 自身のDM
-        my_dms = DmRoom.objects.filter(Member=self.request.user)
-        context['mydms'] = my_dms
-        # 自身のDMに関するメンバー情報の取得
+        my_dms = DmRoom.objects.filter(Member=current_user)
+        # context['room'] = my_dms
+        
+
         other_members = {}
-        for dm in my_dms:
-            other_users = dm.Member.exclude(id=self.request.user.id).values('username', 'profile_img')
-            other_members[dm.id] = list(other_users)
-        context['other_menbers'] = other_members
+        new_message = {}
+        for room in my_dms:
+            other_member = room.Member.exclude(id=current_user.id).first()  # 他のメンバーを取得
+            my_message = Massage.objects.filter(Room=room.id).order_by('Created_at')[:1]
+            if other_member:
+                other_members[room.id] = other_member
+                new_message[room.id] = my_message
+
+        context['rooms'] = my_dms
+        context['other_members'] = other_members
+        context['dmMessage'] = new_message
+
         return context
