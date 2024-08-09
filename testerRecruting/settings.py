a98c057522ab5546f5381d29946e0127e9746608
Yuuya Shimada <blk1012y2@gmail.com>
2024-08-09 17:15:24 +0900

    デプロイ用修正

--- testerRecruting/settings.py
+++ testerRecruting/settings.py
@@ -18,6 +18,8 @@ import logging
 import environ
 from django.contrib import messages
 from google.cloud import secretmanager
+import google.auth
+from google.auth.exceptions import DefaultCredentialsError
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
