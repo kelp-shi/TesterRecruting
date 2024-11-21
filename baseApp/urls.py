534f31737648892ac51b5860e957f7b081c67639
Yuuya Shimada <blk1012y2@gmail.com>
2024-11-22 01:17:05 +0900

    画像ディレクトリ変更

--- baseApp/urls.py
+++ baseApp/urls.py
@@ -58,4 +58,4 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
